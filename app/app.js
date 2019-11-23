9c7976ca74e07e70b6c851455441a5a3e96f9af6
Phong <phongdoan.dev@gmail.com>
2019-11-22 17:38:48 -0800

    third college-input field created

--- app/app.js
+++ app/app.js
@@ -15,36 +15,68 @@ function collegeFormRender() {
   var collegeFeildSetElem = document.createElement('fieldset');
   var collegeLegendElem = document.createElement('legend');
   // var collegeLabelElem = document.createElement('label');
-  // var collegeInputElem = document.createElement('input');
-  var collegeDropDownElem = document.createElement('select');
-  // var collegeOptionElem = document.createElement('option');
-
-  //Creating the form
   collegeSection.appendChild(collegeFormElem);
   collegeFormElem.id = 'stringify';
   collegeFormElem.appendChild(collegeFeildSetElem);
   collegeFeildSetElem.appendChild(collegeLegendElem);
   collegeLegendElem.textContent = 'Life as a Young Adult';
-  collegeFeildSetElem.appendChild(collegeDropDownElem);
+
+  var educationPath = document.createElement('p');
+  educationPath.textContent = 'What educational path did you take?';
+  collegeFeildSetElem.appendChild(educationPath);
 
   var education = ['University', 'Technical School', 'Military', 'None'];
+  var educationDropDownElem = document.createElement('select');
+  collegeFeildSetElem.appendChild(educationDropDownElem);
+
   for (var path = 0; path < education.length; path++) {
     console.log('path :', path);
     var collegeOptionElem = document.createElement('option');
-    collegeDropDownElem.appendChild(collegeOptionElem);
     collegeOptionElem.value = education[path];
     collegeOptionElem.textContent = education[path];
+    educationDropDownElem.appendChild(collegeOptionElem);
   }
 
-  var degreeDropDownElem = document.createElement('select');
+  var degreeFamilyQuestion = document.createElement('p');
+  degreeFamilyQuestion.textContent = 'What educational path did you take?';
+  collegeFeildSetElem.appendChild(degreeFamilyQuestion);
+
   var degreeType = ['Science', 'Arts', 'Math', 'None'];
+  var degreeDropDownElem = document.createElement('select');
+  collegeFeildSetElem.appendChild(degreeDropDownElem);
+
   for (var family = 0; family < degreeType.length; family++) {
     var degreeOptionElem = document.createElement('option');
-    collegeFeildSetElem.appendChild(degreeDropDownElem);
-    degreeDropDownElem.appendChild(degreeOptionElem);
     degreeOptionElem.value = degreeType[family];
     degreeOptionElem.textContent = degreeType[family];
+    degreeDropDownElem.appendChild(degreeOptionElem);
   }
+
+  var maritalQuestion = document.createElement('p');
+  maritalQuestion.textContent = 'Are you married?';
+  collegeFeildSetElem.appendChild(maritalQuestion);
+
+  var maritalQuestionTrue = document.createElement('p');
+  maritalQuestionTrue.textContent = 'Yes';
+  maritalQuestion.appendChild(maritalQuestionTrue);
+
+  var isMarried = document.createElement('input');
+  isMarried.type = 'radio';
+  isMarried.name = 'maritalStatus';
+  isMarried.value = 'yes';
+  maritalQuestionTrue.appendChild(isMarried);
+
+  var maritalQuestionFalse = document.createElement('p');
+  maritalQuestionFalse.textContent = 'No';
+  maritalQuestion.appendChild(maritalQuestionFalse);
+
+  var isNotMarried = document.createElement('input');
+  isNotMarried.type = 'radio';
+  isNotMarried.name = 'maritalStatus';
+  isNotMarried.value = 'no';
+  maritalQuestionFalse.appendChild(isNotMarried);
+
+
 
   // var possibleMaritialAnswers = ['yes', 'no'];
   // for (var i = 0; i < possibleMaritialAnswers.length; i++) {
