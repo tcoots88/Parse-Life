2b28e5f5710d8f4244eb2f62a5c0166c64e781c3
Phong <phongdoan.dev@gmail.com>
2019-11-26 12:16:52 -0800

    set required on inputs

--- app/app.js
+++ app/app.js
@@ -21,6 +21,7 @@ var createElementaryForm = function () {
   var questionOneDrop = document.createElement('select');
   questionOneDrop.setAttribute('class', 'dropDowns');
   questionOneDrop.name = 'favoriteSubject';
+  questionOneDrop.required = true;
   questionOne.appendChild(questionOneDrop);
   var subjects = ['Math', 'literature', 'Science', 'Art'];
   for (var subjectOptions = 0; subjectOptions < subjects.length; subjectOptions++) {
@@ -35,6 +36,7 @@ var createElementaryForm = function () {
   elementaryFieldSet.appendChild(questionTwo);
   questionTwo.textContent = 'What were your favorite things to do?';
   var questionTwoDrop = document.createElement('select');
+  questionTwoDrop.required = true;
   questionTwoDrop.name = 'hobbiesAsKid';
   questionTwo.appendChild(questionTwoDrop);
   var ThingsToDO = ['Sports', 'Pick Your Nose', 'Play Video Games', 'Fight With Siblings', 'Watch TV'];
@@ -49,6 +51,7 @@ var createElementaryForm = function () {
   elementaryFieldSet.appendChild(questionThree);
   questionThree.textContent = 'Were you sent to the principals office?';
   var questionThreeDrop = document.createElement('select');
+  questionThreeDrop.required = true;
   questionThreeDrop.name = 'troubled';
   questionThree.appendChild(questionThreeDrop);
   var questionThreeChoices = ['Yes', 'No'];
@@ -67,6 +70,7 @@ var createElementaryForm = function () {
   questionFourField.setAttribute('class', 'inputField');
   questionFourField.type = 'text';
   questionFourField.name = 'cartoon';
+  questionFourField.required = true;
   questionFour.appendChild(questionFourField);
 
   formSubmission(elementaryFieldSet, 'elemetaryAnswers');
@@ -121,7 +125,7 @@ function hsFormRender() {
   //  switched variables to global
   var hsSection = document.getElementById('hs');
   var hsFormElem = document.createElement('form');
-  hsFormElem.id = 'stringify';
+  hsFormElem.id = 'style';
   var hsFieldSetElem = document.createElement('fieldset');
   var hsLegendElem = document.createElement('legend');
   hsLegendElem.textContent = 'Life as a High School Student';
@@ -141,6 +145,7 @@ function hsFormRender() {
   var hsCliqueArray = ['Populars', 'Jocks', 'Good-Ats', 'Hipsters', 'The Brains', 'Normals', 'Stoners', 'Emos/Goths', 'Anime/Manga Fans', 'Loners', 'None of the above'];
   var cliqueDropDownElem = document.createElement('select');
   cliqueDropDownElem.name = 'hsCliqueMember';
+  cliqueDropDownElem.required = true;
   hsFieldSetElem.appendChild(cliqueDropDownElem);
 
   for (var path = 0; path < hsCliqueArray.length; path++) {
@@ -163,7 +168,6 @@ function hsFormRender() {
 
   for (var activity = 0; activity < hsActivityArray.length; activity++) {
     var activityOptionElem = document.createElement('option');
-    activityOptionElem.name = 'hsECActivity';
     activityOptionElem.value = hsActivityArray[activity];
     activityOptionElem.textContent = hsActivityArray[activity];
     activityDropDownElem.appendChild(activityOptionElem);
@@ -182,6 +186,7 @@ function hsFormRender() {
   hsLanguageLearned.type = 'radio';
   hsLanguageLearned.name = 'learnedLanguage';
   hsLanguageLearned.value = 'yes';
+  hsLanguageLearned.required = true;
   hsLanguageQuestionTrue.appendChild(hsLanguageLearned);
 
   var hsLanguageQuestionFalse = document.createElement('p');
@@ -207,6 +212,7 @@ function hsFormRender() {
   hsRelationship.type = 'radio';
   hsRelationship.name = 'relationship';
   hsRelationship.value = 'yes';
+  hsRelationship.required = true;
   hsRelationshipQuestionTrue.appendChild(hsRelationship);
 
   var hsRelationshipQuestionFalse = document.createElement('p');
@@ -233,6 +239,7 @@ function hsFormRender() {
   hsDiploma.type = 'radio';
   hsDiploma.name = 'diploma';
   hsDiploma.value = 'yes';
+  hsDiploma.required = true;
   hsDiplomaQuestionTrue.appendChild(hsDiploma);
 
   var hsDiplomaQuestionFalse = document.createElement('p');
@@ -275,7 +282,6 @@ function submitHandlerHighSchool(event) {
     hsGlobalArray.push(addArray);
 
   }
-  console.log('hsGlobalArray: ', hsGlobalArray);
   event.target.reset();
 
 }
@@ -312,6 +318,7 @@ function collegeFormRender() {
   var education = ['University', 'Technical School', 'Military', 'None'];
   var educationDropDownElem = document.createElement('select');
   educationDropDownElem.name = 'educationPath';
+  educationDropDownElem.required = true;
   educationPath.appendChild(educationDropDownElem);
 
 
@@ -330,6 +337,7 @@ function collegeFormRender() {
   var degreeType = ['Science', 'Arts', 'Math', 'None'];
   var degreeDropDownElem = document.createElement('select');
   degreeDropDownElem.name = 'degreetype';
+  degreeDropDownElem.required = true;
   degreeFamilyQuestion.appendChild(degreeDropDownElem);
 
   for (var family = 0; family < degreeType.length; family++) {
@@ -353,6 +361,7 @@ function collegeFormRender() {
   isMarried.type = 'radio';
   isMarried.name = 'maritalStatus';
   isMarried.value = 'yes';
+  isMarried.required = true;
   maritalQuestionTrue.appendChild(isMarried);
 
   var maritalQuestionFalse = document.createElement('p');
@@ -378,6 +387,7 @@ function collegeFormRender() {
   hasBeliefs.type = 'radio';
   hasBeliefs.name = 'beliefs';
   hasBeliefs.value = 'yes';
+  hasBeliefs.required = true;
   beliefsQuestionTrue.appendChild(hasBeliefs);
 
   var beliefsQuestionFalse = document.createElement('p');
@@ -416,6 +426,7 @@ function careerFormRender() {
   var career = ['Military', 'Buisness', 'Engineer', 'Finance', 'Skill Trade', 'Other'];
   var careerDropDownElem = document.createElement('select');
   careerDropDownElem.name = 'careerPath';
+  careerDropDownElem.required = true;
   careerPath.appendChild(careerDropDownElem);
 
   for (var path = 0; path < career.length; path++) {
@@ -433,6 +444,7 @@ function careerFormRender() {
   var hobbiesAsAdultText = document.createElement('input');
   hobbiesAsAdultText.name = 'adultHobbies';
   hobbiesAsAdultText.type = 'text';
+  hobbiesAsAdultText.required = true;
   adultHobbiesQuestion.appendChild(hobbiesAsAdultText);
 
 
@@ -449,6 +461,7 @@ function careerFormRender() {
   isMarriedWithKids.type = 'radio';
   isMarriedWithKids.name = 'maritalStatusAdult';
   isMarriedWithKids.value = 'yesKids';
+  isMarriedWithKids.required = true;
   maritalQuestionTrueWithKids.appendChild(isMarriedWithKids);
 
   var maritalQuestionTrue = document.createElement('p');
@@ -479,6 +492,7 @@ function careerFormRender() {
   var regionAdultField = document.createElement('input');
   regionAdultField.name = 'regionAsAdult';
   regionAdultField.type = 'text';
+  regionAdultField.required = true;
 
   regionAdultElement.appendChild(regionAdultField);
 
@@ -825,3 +839,4 @@ deathForm.addEventListener('submit', deathHandler);
 
 //
 
+// function
