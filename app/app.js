1eca83b85a650c3f08a90078b8524f7a2dd5121a
Phong <phongdoan.dev@gmail.com>
2019-11-25 19:01:15 -0800

    Commented out CSS and correct linter

--- app/app.js
+++ app/app.js
@@ -251,7 +251,7 @@ var birthGlobalArray = [];
 var birthDivElement = document.getElementById('birth');
 
 var birthForm = document.createElement('form');
-birthForm.id = 'birthForn'
+birthForm.id = 'birthForn';
 birthDivElement.appendChild(birthForm);
 
 var birthFieldset = document.createElement('fieldset');
@@ -274,11 +274,11 @@ seasonChoice.name = 'seasonDDValue';
 seasonChoice.required = true;
 birthSeason.appendChild(seasonChoice);
 
-for(var season = 0; season < seasonArray.length; season++){
-    var option = document.createElement('option');
-    option.value = seasonArray[season];
-    option.text = seasonArray[season];
-    seasonChoice.appendChild(option);
+for (var season = 0; season < seasonArray.length; season++) {
+  var seasonOption = document.createElement('option');
+  seasonOption.value = seasonArray[season];
+  seasonOption.text = seasonArray[season];
+  seasonChoice.appendChild(seasonOption);
 }
 //---------------------------------------------Input 2 -----------------------------------
 var birthName = document.createElement('p');
@@ -306,11 +306,11 @@ genderChoice.required = true;
 
 birthGender.appendChild(genderChoice);
 
-for(var gender = 0; gender < genderArray.length; gender++){
-    var option = document.createElement('option');
-    option.value = genderArray[gender];
-    option.text = genderArray[gender];
-    genderChoice.appendChild(option);
+for (var gender = 0; gender < genderArray.length; gender++) {
+  var option = document.createElement('option');
+  option.value = genderArray[gender];
+  option.text = genderArray[gender];
+  genderChoice.appendChild(option);
 }
 
 //---------------------------------------------- Input 4 ---------------------------------------
@@ -342,13 +342,12 @@ var deathGlobalArray = [];
 var deathDivElement = document.getElementById('death');
 
 var deathForm = document.createElement('form');
-deathForm.id = 'deathForm'
+deathForm.id = 'deathForm';
 deathDivElement.appendChild(deathForm);
 
 var deadFieldset = document.createElement('fieldset');
 deathForm.appendChild(deadFieldset);
 
-var deadLegend = document.createElement('legend');
 deadLegend.textContent = '';
 deadFieldset.appendChild(deadLegend);
 
@@ -366,11 +365,11 @@ burialChoice.required = true;
 deathCeremonyType.appendChild(burialChoice);
 
 
-for(var buryType = 0; buryType < burialArray.length; buryType++){
-    var buryOption = document.createElement('option');
-    buryOption.value = burialArray[buryType];
-    buryOption.text = burialArray[buryType];
-    burialChoice.appendChild(buryOption);
+for (var buryType = 0; buryType < burialArray.length; buryType++) {
+  var buryOption = document.createElement('option');
+  buryOption.value = burialArray[buryType];
+  buryOption.text = burialArray[buryType];
+  burialChoice.appendChild(buryOption);
 }
 //---------------------------------------------Input 2 -----------------------------------
 var retirementDestination = document.createElement('p');
@@ -396,20 +395,20 @@ bucketSelection.name = 'bucketDDValue';
 bucketSelection.required = true;
 bucketList.appendChild(bucketSelection);
 
-for(var bucketOption = 0; bucketOption < bucketArray.length; bucketOption++){
-    var dropOption = document.createElement('option');
-    dropOption.value = bucketArray[bucketOption];
-    dropOption.text = bucketArray[bucketOption];
-    bucketSelection.appendChild(dropOption);
+for (var bucketOption = 0; bucketOption < bucketArray.length; bucketOption++) {
+  var dropOption = document.createElement('option');
+  dropOption.value = bucketArray[bucketOption];
+  dropOption.text = bucketArray[bucketOption];
+  bucketSelection.appendChild(dropOption);
 }
 
 //---------------------------------------------- Input 4 ---------------------------------------
 var fullfillmentArticle = document.createElement('article');
 fullfillmentArticle.textContent = 'What will provide you fullfillment? ';
-var breakAdd = document.createElement('break');
+// var breakAdd = document.createElement('break');
 deadFieldset.appendChild(fullfillmentArticle);
 
-var financialFulfillmentLabel  = document.createElement('p');
+var financialFulfillmentLabel = document.createElement('p');
 financialFulfillmentLabel.textContent = 'Financial';
 fullfillmentArticle.appendChild(financialFulfillmentLabel);
 
@@ -417,10 +416,10 @@ var financialFulfillment = document.createElement('input');
 financialFulfillment.type = 'checkbox';
 financialFulfillment.name = 'financial';
 financialFulfillment.value = 'Financial';
-financialFulfillment.id = "financialID";
+financialFulfillment.id = 'financialID';
 financialFulfillmentLabel.appendChild(financialFulfillment);
 
-var marriageFulfillmentLabel  = document.createElement('p');
+var marriageFulfillmentLabel = document.createElement('p');
 marriageFulfillmentLabel.textContent = 'Marriage';
 fullfillmentArticle.appendChild(marriageFulfillmentLabel);
 
@@ -431,7 +430,7 @@ marriageFulfillment.value = 'Marriage';
 marriageFulfillment.id = 'marriageID';
 marriageFulfillmentLabel.appendChild(marriageFulfillment);
 
-var childrenFulfillmentLabel  = document.createElement('p');
+var childrenFulfillmentLabel = document.createElement('p');
 childrenFulfillmentLabel.textContent = 'Children';
 fullfillmentArticle.appendChild(childrenFulfillmentLabel);
 
@@ -442,7 +441,7 @@ childrenFulfillment.value = 'Children';
 childrenFulfillment.id = 'childrenID';
 childrenFulfillmentLabel.appendChild(childrenFulfillment);
 
-var careerFulfillmentLabel  = document.createElement('p');
+var careerFulfillmentLabel = document.createElement('p');
 careerFulfillmentLabel.textContent = 'Career';
 fullfillmentArticle.appendChild(careerFulfillmentLabel);
 
@@ -454,47 +453,47 @@ careerFulfillment.id = 'careerID';
 careerFulfillmentLabel.appendChild(careerFulfillment);
 
 //------------------------------------------------------------------------------------------------
-function birthHandler(event){
-    event.preventDefault();
+function birthHandler(event) {
+  event.preventDefault();
 
-    var birthNameText = event.target.birthNameFieldValue.value;
-    var seasonDropDownValue = event.target.seasonDDValue.value;
-    var genderDropDownValue = event.target.genderDDValue.value;
-    var regionText = event.target.regionFieldValue.value;
+  var birthNameText = event.target.birthNameFieldValue.value;
+  var seasonDropDownValue = event.target.seasonDDValue.value;
+  var genderDropDownValue = event.target.genderDDValue.value;
+  var regionText = event.target.regionFieldValue.value;
 
 
-    birthGlobalArray.push(birthNameText);
-    birthGlobalArray.push(seasonDropDownValue);
-    birthGlobalArray.push(genderDropDownValue);
-    birthGlobalArray.push(regionText);
+  birthGlobalArray.push(birthNameText);
+  birthGlobalArray.push(seasonDropDownValue);
+  birthGlobalArray.push(genderDropDownValue);
+  birthGlobalArray.push(regionText);
 
-    console.log(birthNameText);
-    console.log(seasonDropDownValue);
-    console.log(genderDropDownValue);
-    console.log(regionText);
+  console.log(birthNameText);
+  console.log(seasonDropDownValue);
+  console.log(genderDropDownValue);
+  console.log(regionText);
 
-    event.target.reset();
+  event.target.reset();
 }
 
-function deathHandler(event){
-    event.preventDefault();
+function deathHandler(event) {
+  event.preventDefault();
 
-    var burialDropDownValue = event.target.burialDDValue.value;
-    var retirementText = event.target.retirementFieldValue.value;
-    var bucketDropDownValue = event.target.bucketDDValue.value;
-    var fullfillmentCheck = arrayOfChecked;
+  var burialDropDownValue = event.target.burialDDValue.value;
+  var retirementText = event.target.retirementFieldValue.value;
+  var bucketDropDownValue = event.target.bucketDDValue.value;
+  var fullfillmentCheck = arrayOfChecked;
 
-    deathGlobalArray.push(burialDropDownValue);
-    deathGlobalArray.push(retirementText);
-    deathGlobalArray.push(bucketDropDownValue);
-    deathGlobalArray.push(fullfillmentCheck);
+  deathGlobalArray.push(burialDropDownValue);
+  deathGlobalArray.push(retirementText);
+  deathGlobalArray.push(bucketDropDownValue);
+  deathGlobalArray.push(fullfillmentCheck);
 
-    console.log(burialDropDownValue);
-    console.log(retirementText);
-    console.log(bucketDropDownValue);
-    console.log(fullfillmentCheck);
+  console.log(burialDropDownValue);
+  console.log(retirementText);
+  console.log(bucketDropDownValue);
+  console.log(fullfillmentCheck);
 
-    event.target.reset();
+  event.target.reset();
 }
 
 document.getElementById('financialID').onclick = checkBoxCheck;
@@ -504,12 +503,12 @@ document.getElementById('careerID').onclick = checkBoxCheck;
 
 var arrayOfChecked = [];
 
-function checkBoxCheck(){
-    console.log(this);
-    var arrayOfIDs = ['financialID', 'marriageID', 'childrenID', 'careerID'];
+function checkBoxCheck() {
+  console.log(this);
+  // var arrayOfIDs = ['financialID', 'marriageID', 'childrenID', 'careerID'];
 
-    arrayOfChecked.push(this.value);
-    console.log(arrayOfChecked);
+  arrayOfChecked.push(this.value);
+  console.log(arrayOfChecked);
 }
 
 
